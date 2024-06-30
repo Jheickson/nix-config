b307d4250733fe42f5db9b077ede67d521aed5c4
Jheickson <felipesantosatm@outlook.com>
2024-06-30 00:46:46 -0300

    Flake attempt

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "My nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-stable, home-manager, ... }@inputs:
+
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+    # nixos - system hostname
+    nixosConfigurations.felipe = nixpkgs.lib.nixosSystem {
+      specialArgs = {
+        pkgs-stable = import nixpkgs-stable {
+          inherit system;
+          config.allowUnfree = true;
+        };
+        inherit inputs system;
+      };
+      modules = [
+        ./nixos/configuration.nix
+      ];
+    };
+
+  };
+}
