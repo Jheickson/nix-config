6feac9a2e725ce8f31fa1c73d09e86c5e64dfb0b
Jheickson <felipesantosatm@outlook.com>
2024-07-01 08:53:59 -0300

    Stylix: Change wallpaper

--- nixos/stylix.nix
+++ nixos/stylix.nix
@@ -9,10 +9,10 @@
 
     polarity = "dark";
 
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
+    # base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
 
     # It can also be generated from an inmage
-    image = ./wallpapers/Themed/catpuccin_tetris.png;
+    image = ./wallpapers/Landscape/wallhaven-jxqrw5_3840x2160.png;
 
   };
 
