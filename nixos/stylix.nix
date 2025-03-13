4e6c4cb0fe39dd743cd4238c1c08299e77f1a5cb
Jheickson <felipesantosatm@outlook.com>
2025-03-13 16:27:31 -0300

    Stylix: Wallpaper && Colorscheme

--- nixos/stylix.nix
+++ nixos/stylix.nix
@@ -54,10 +54,10 @@
       size = 8;
     };
 
-    # base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/oceanicnext.yaml";
 
     # It can also be generated from an image
-    image = ./wallpapers/Aesthetic/wallhaven-yxk33x.jpg;
+    image = ./wallpapers/Minimalistic/wallhaven-1pzdmg.png;
   };
 
   fonts = {
