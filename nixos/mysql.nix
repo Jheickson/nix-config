20a01b4cc3ba891ff2807ab0b16c62c6df6169e2
Jheickson <felipesantosatm@outlook.com>
2025-05-20 10:23:12 -0300

    Config: MySQL pro Alugon

--- nixos/mysql.nix
+++ nixos/mysql.nix
@@ -1,31 +1,34 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
+  # 1) Enable MariaDB (MySQL) service
   services.mysql = {
-    enable = true;
-    package = pkgs.mariadb;
-
+    enable        = true;
+    package       = pkgs.mariadb;
+    # Automatically run this SQL script when the database is first initialized:
+    initialScript = "/etc/nixos/aluga_banco.sql";
+    # (only runs if the data directory is empty)
   };
-  services = {
 
-    httpd.enable = true;
-    # httpd.adminAddr = "webmaster@example.org";
-    httpd.enablePHP = true; # oof... not a great idea in my opinion
-    httpd.adminAddr = "admin@admin.com"; # Set your admin email
-    # httpd.documentRoot = "/var/www/html";  # Set your document root
-    httpd.virtualHosts = {
+  # 2) Your existing HTTPD/PHP config, now using flat attribute paths
+  services.httpd = {
+    enable      = true;
+    enablePHP   = true;
+    adminAddr   = "admin@admin.com";
+    virtualHosts = {
       "bar.example.com" = {
-        # addSSL = true;
         documentRoot = "/var/www/bar.example.com";
       };
     };
   };
 
+  # 3) Add your PHP tooling to the system path
   environment.systemPackages = with pkgs; [
     php
     phpactor
     php84Packages.composer
   ];
 
-  networking.firewall.allowedTCPPorts = [ 80 ];
+  # 4) Open port 80 in the firewall
+  networking.firewall.allowedTCPPorts = [ 80 3306];
 }
