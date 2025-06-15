547298e6d56ff53878d195f3c46c5a6301d50162
Jheickson <felipesantosatm@outlook.com>
2025-06-15 17:03:02 -0300

    Config: Vial.nix

---
+++ nixos/vial.nix
@@ -0,0 +1,15 @@
+{ config, pkgs, ... }:
+
+let
+  vialUdevRules = pkgs.writeTextFile {
+    name        = "vial-udev-rules";
+    destination = "/etc/udev/rules.d/60-vial.rules";
+    text = ''
+      # Vial keyboard (vendor 0xFEED, product 0x1212)
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="feed", ATTRS{idProduct}=="1212",
+        MODE="0660", TAG+="uaccess"
+    '';
+  };
+in {
+  services.udev.packages = [ vialUdevRules ];
+}
