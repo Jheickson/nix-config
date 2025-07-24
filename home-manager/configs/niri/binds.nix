3a5eabcb1d86d7bf25c194ed78015a9f898585eb
Jheickson Felipe <felipesantosatm@outlook.com>
2025-07-24 19:13:01 -0300

    niri: replace brillo with brightnessctl for brightness keys

--- home-manager/configs/niri/binds.nix
+++ home-manager/configs/niri/binds.nix
@@ -8,7 +8,7 @@
     with config.lib.niri.actions;
     let
       set-volume = spawn "wpctl" "set-volume" "@DEFAULT_AUDIO_SINK@";
-      brillo = spawn "${pkgs.brillo}/bin/brillo" "-q" "-u" "300000";
+      brightnessctl = spawn "${pkgs.brightnessctl}/bin/brightnessctl";
       playerctl = spawn "${pkgs.playerctl}/bin/playerctl";
     in
     {
@@ -23,8 +23,8 @@
       "XF86AudioRaiseVolume".action = set-volume "5%+";
       "XF86AudioLowerVolume".action = set-volume "5%-";
 
-      "XF86MonBrightnessUp".action = brillo "-A" "5";
-      "XF86MonBrightnessDown".action = brillo "-U" "5";
+      "XF86MonBrightnessUp".action = brightnessctl "set" "+5%";
+      "XF86MonBrightnessDown".action = brightnessctl "set" "5%-";
 
       "Print".action.screenshot-screen = {
         write-to-disk = true;
