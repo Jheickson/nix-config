409e0c6722112474c47bede8c25a41e41ed8d27f
Jheickson Felipe <felipesantosatm@outlook.com>
2025-08-02 18:42:07 -0300

    wine update

--- nixos/wine.nix
+++ nixos/wine.nix
@@ -1,9 +1,6 @@
 { config, pkgs, ... }:
 
-{
-  # Enable Wine
-  programs.wine.enable = true;
-  
+{ 
   # Add wine to system packages
   environment.systemPackages = with pkgs; [
     wineWowPackages.full
