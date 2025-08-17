1347312a2b7440d759a07f1b220f31557867e025
Jheickson Felipe <felipesantosatm@outlook.com>
2025-08-17 17:31:07 -0300

    fix(vial): Update udev rules for better keyboard detection

--- nixos/vial.nix
+++ nixos/vial.nix
@@ -3,10 +3,10 @@
 let
   vialUdevRules = pkgs.writeTextFile {
     name = "vial-udev-rules";
-    destination = "/etc/udev/rules.d/60-vial.rules";
+    destination = "/etc/udev/rules.d/99-vial.rules";
     text = ''
       # Vial keyboard (vendor 0xFEED, product 0x1212)
-      SUBSYSTEM=="usb", ATTRS{idVendor}=="feed", ATTRS{idProduct}=="1212" MODE="0660", TAG+="uaccess"
+      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{serial}=="*vial:f64c2b3c*", ATTRS{idVendor}=="feed", ATTRS{idProduct}=="1212", MODE="0660", GROUP="users", TAG+="uaccess", TAG+="udev-acl"
     '';
   };
 in
