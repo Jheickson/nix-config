5f24cdb36c2d000ad4fd1e17100421cb668741aa
Jheickson Felipe <felipesantosatm@outlook.com>
2025-10-14 15:06:13 -0300

    fix(upower): lower battery thresholds for low and critical levels
    Adjust percentageLow from 25 to 20 and percentageCritical from 15 to 10
    to provide earlier warnings for low battery situations.

--- nixos/upower.nix
+++ nixos/upower.nix
@@ -4,8 +4,8 @@
   services.upower = {
     enable = true;
 
-    percentageLow = 25;
-    percentageCritical = 15;
+    percentageLow = 20;
+    percentageCritical = 10;
     percentageAction = 5;
     criticalPowerAction = "HybridSleep"; # Options: PowerOff, Hibernate, HybridSleep, None
 
