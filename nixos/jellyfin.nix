041675e81eb893c28d96a26348a5848352b25a90
Jheickson Felipe <felipesantosatm@outlook.com>
2025-10-15 17:04:51 -0300

    fix(jellyfin.nix): comment out systemd service override for ExecStart

--- nixos/jellyfin.nix
+++ nixos/jellyfin.nix
@@ -9,10 +9,10 @@
     user="felipe";
   };
 
-  # Override the systemd service to add the --service flag
+  /* # Override the systemd service to add the --service flag
   systemd.services.jellyfin.serviceConfig.ExecStart = lib.mkForce
     "${config.services.jellyfin.package}/bin/jellyfin --service --datadir '/var/lib/jellyfin' --configdir '/var/lib/jellyfin/config' --cachedir '/var/cache/jellyfin' --logdir '/var/lib/jellyfin/log'";
-
+ */
   environment.systemPackages = [
     pkgs.jellyfin
     pkgs.jellyfin-web
