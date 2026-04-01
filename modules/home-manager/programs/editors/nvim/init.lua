2b376ef6d27b0cf07b93b2f8b5c2c934f21ba525
Jheickson Felipe <felipesantosatm@outlook.com>
2026-04-01 16:12:56 -0300

    feat(opencode): add input configuration and update key mappings for opencode actions

--- modules/home-manager/programs/editors/nvim/init.lua
+++ modules/home-manager/programs/editors/nvim/init.lua
@@ -229,6 +229,7 @@ require('auto-session').setup({
 
 require("snacks").setup({
   explorer = {},
+  input = {},
   picker = {
     actions = {
       opencode_send = function(...)
@@ -268,13 +269,13 @@ vim.g.opencode_opts = {}
 
 vim.o.autoread = true
 
-vim.keymap.set({ 'n', 'x' }, '<leader>aa', function()
+vim.keymap.set({ 'n', 'x' }, '<C-a>', function()
   require('opencode').ask('@this: ', { submit = true })
 end, { desc = 'Ask opencode' })
-vim.keymap.set({ 'n', 'x' }, '<leader>ac', function()
+vim.keymap.set({ 'n', 'x' }, '<C-x>', function()
   require('opencode').select()
-end, { desc = 'Open opencode actions' })
-vim.keymap.set({ 'n', 't' }, '<leader>ai', function()
+end, { desc = 'Execute opencode action' })
+vim.keymap.set({ 'n', 't' }, '<C-.>', function()
   require('opencode').toggle()
 end, { desc = 'Toggle opencode' })
 vim.keymap.set({ 'n', 'x' }, 'go', function()
@@ -283,6 +284,12 @@ end, { desc = 'Add range to opencode', expr = true })
 vim.keymap.set('n', 'goo', function()
   return require('opencode').operator('@this ') .. '_'
 end, { desc = 'Add line to opencode', expr = true })
+vim.keymap.set('n', '<S-C-u>', function()
+  require('opencode').command('session.half.page.up')
+end, { desc = 'Scroll opencode up' })
+vim.keymap.set('n', '<S-C-d>', function()
+  require('opencode').command('session.half.page.down')
+end, { desc = 'Scroll opencode down' })
 
 vim.keymap.set("n", "-", function() Snacks.explorer.open() end, { desc = 'Snacks Explorer' })
 vim.keymap.set("n", "<c-\\>", function() Snacks.terminal.open() end, { desc = 'Snacks Terminal' })
