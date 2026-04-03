39999ca8345b0e965129be08691859c03c593e64
Jheickson Felipe <felipesantosatm@outlook.com>
2026-04-03 18:50:26 -0300

    feat(stylix): update wallpaper source and theme file

--- modules/shared/stylix-settings.nix
+++ modules/shared/stylix-settings.nix
@@ -4,11 +4,11 @@
   enable = true;
   useThemeFile = true;
 
-  wallpaperSource = ../../assets/wallpapers/Minimalistic/wallhaven-9omylw.png;
+  wallpaperSource = ../../assets/wallpapers/Minimalistic/wallhaven-288w19.png;
   wallpaperImage = ../../assets/wallpapers/wallpaper.png;
   wallpaperOutputPath = "/home/felipe/nix-config/assets/wallpapers/wallpaper.png";
 
-  themeFile = "${pkgs.base16-schemes}/share/themes/rose-pine.yaml";
+  themeFile = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
 }
 
 /* 
