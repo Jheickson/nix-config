36131c4a23e8ce52304f9e17266936c3cb5bf7fa
Jheickson Felipe <felipesantosatm@outlook.com>
2026-04-30 14:29:27 -0300

    fix(stylix-settings): update wallpaper source and theme file for improved aesthetics

--- modules/shared/stylix-settings.nix
+++ modules/shared/stylix-settings.nix
@@ -4,11 +4,11 @@
   enable = true;
   useThemeFile = true;
 
-  wallpaperSource = ../../assets/wallpapers/Landscape/wallhaven-o5p9wl.png;
+  wallpaperSource = ../../assets/wallpapers/Landscape/wallhaven-ml3jm8.jpg;
   wallpaperImage = ../../assets/wallpapers/wallpaper.png;
   wallpaperOutputPath = "/home/felipe/nix-config/assets/wallpapers/wallpaper.png";
 
-  themeFile = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
+  themeFile = "${pkgs.base16-schemes}/share/themes/horizon-dark.yaml";
 }
 
 /* 
