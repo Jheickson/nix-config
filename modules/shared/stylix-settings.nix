aff63305e18481e454209a5dfac2d193e29529ba
Jheickson Felipe <felipesantosatm@outlook.com>
2026-06-25 21:50:58 -0300

    fix: update theme file to use emil.yaml in stylix-settings.nix

--- modules/shared/stylix-settings.nix
+++ modules/shared/stylix-settings.nix
@@ -18,7 +18,7 @@
 
   wallpaperOutputPath = "/home/felipe/nix-config/assets/wallpapers/wallpaper.png";
 
-  themeFile = "${pkgs.base16-schemes}/share/themes/horizon-light.yaml";
+  themeFile = "${pkgs.base16-schemes}/share/themes/emil.yaml";
 }
 
 /*
