d2c5abbdb75f28af550ea8bb7c1002761f9318e0
Jheickson Felipe <felipesantosatm@outlook.com>
2026-09-07 20:29:23 -0300

    feat(navidrome): add lyrics plugin

--- modules/nixos/services/navidrome/navidrome.nix
+++ modules/nixos/services/navidrome/navidrome.nix
@@ -1,15 +1,33 @@
 { pkgs, lib, ... }:
 
+let
+  ndLyrics = pkgs.stdenvNoCC.mkDerivation {
+    pname = "nd-lyrics";
+    version = "8.0.0";
+    src = ./plugins/nd-lyrics.ndp;
+    dontUnpack = true;
+    installPhase = ''
+      install -Dm644 "$src" "$out/share/$pname.ndp"
+    '';
+    passthru.isNavidromePlugin = true;
+    meta = {
+      description = "Navidrome plugin for fetching lyrics from multiple sources";
+      homepage = "https://github.com/J0R6IT0/navidrome-lyrics-plugin";
+      license = lib.licenses.mit;
+    };
+  };
+in
 {
-
   services.navidrome = {
     enable = true;
     openFirewall = true;
     user = "felipe";
+    plugins = [ ndLyrics ];
     settings = {
       MusicFolder = "/home/felipe/Music";
       Port = 4533;
       Address = "0.0.0.0";
+      LyricsPriority = "nd-lyrics,.ttml,.yaml,.yml,.elrc,.lrc,.srt,.txt,embedded";
       LastFM.Enabled = true;
     };
     environmentFile = "/etc/navidrome/secrets.env";
