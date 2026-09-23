a753ac9a4aaebfa36b1b2fcb958f48a04ba00fb2
Jheickson Felipe <felipesantosatm@outlook.com>
2026-09-23 03:22:41 -0300

    fix(nvim): use bf for buffer finder

--- modules/home-manager/programs/editors/nvim/vanilla/init.lua
+++ modules/home-manager/programs/editors/nvim/vanilla/init.lua
@@ -715,7 +715,7 @@ require("mini.pick").setup({
 })
 vim.keymap.set("n", "<leader>ff", MiniPick.builtin.files, { desc = "Find files" })
 vim.keymap.set("n", "<leader>fg", MiniPick.builtin.grep_live, { desc = "Search in project" })
-vim.keymap.set("n", "<leader>fb", MiniPick.builtin.buffers, { desc = "Open buffers" })
+vim.keymap.set("n", "<leader>bf", MiniPick.builtin.buffers, { desc = "Open buffers" })
 vim.keymap.set("n", "<leader>fh", MiniPick.builtin.help, { desc = "Help" })
 vim.keymap.set("n", "<leader>fd", function()
 	MiniPick.builtin.diagnostic({ scope = "all" })
