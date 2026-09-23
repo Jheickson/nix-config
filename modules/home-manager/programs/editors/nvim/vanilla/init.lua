1323875fe8e9c3177785ad74084e50b122221b96
Jheickson Felipe <felipesantosatm@outlook.com>
2026-09-23 03:06:22 -0300

    feat(nvim): preview markdown images at cursor

--- modules/home-manager/programs/editors/nvim/vanilla/init.lua
+++ modules/home-manager/programs/editors/nvim/vanilla/init.lua
@@ -311,6 +311,8 @@ if image_ok then
 			markdown = {
 				enabled = true,
 				download_remote_images = true,
+				only_render_image_at_cursor = true,
+				only_render_image_at_cursor_mode = "inline",
 				filetypes = { "markdown", "vimwiki" },
 			},
 		},
